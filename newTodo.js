d2427f4e6cc1a558bdcc007a9dfe4e7ca0d867aa
salman k <salman371k@gmail.com>
2024-04-15 12:35:23 +0530

    change

--- newTodo.js
+++ newTodo.js
@@ -2,6 +2,7 @@ const http = require('http')
 const fs = require("fs")
 
 
+const readfile = fs.readFileSync('Tods.json','utf8')
 
 
 const server =  http.createServer((req,res)=>{
@@ -9,17 +10,40 @@ const Method  = req.method
 const Url = req.url
 
 if(Url === '/Todos' && Method === 'GET'){
-    fs.readFile('Todo.json','utf8',(err,data)=>{
-        if(err) throw err
         res.writeHead(200,{"content-type":"application/json"})
-        res.end(data)
+        res.end(readfile)
+}
+if(Url === '/Todos' && Method === 'POST'){
+    let pdata =''
+    req.on('data',(chank)=>{
+       pdata+=chank
     })
+    req.on('end',()=>{
+        console.log(pdata);
+        const readfile = fs.readFileSync('Tods.json','utf8')
+        console.log(readfile);
+        const newre = JSON.parse(readfile)
+        const body =JSON.parse(pdata)
+        console.log(body);
+        const newdata= [...newre,body]
+        console.log(newdata)
+        console.log(JSON.stringify(newdata));
+        fs.writeFileSync('Tods.json',JSON.stringify(newdata))
+        res.writeHead(201,{'content-type':'application/json'})
+        res.end(JSON.stringify(newdata))
+    })
+}
+if(Url === '/Todos' && Method === 'DELETE'){
+    const readfile = fs.readFileSync('Tods.json','utf8')
+
+  
 }
 
 })
 
 
 const PORT = 4005
+
 server.listen(PORT,()=>{
-    `server iss renning ${PORT}`
+    console.log(`server iss renning ${PORT}`);
 })
